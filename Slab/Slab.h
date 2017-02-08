f3e8256a55cfdff2d1df96e7219caf928848c4d7
Nandiin <nandiin@163.com>
2017-02-09 00:40:26 +0800

    new project

---
+++ Slab/Slab.h
@@ -0,0 +1,19 @@
+//
+//  Slab.h
+//  Slab
+//
+//  Created by 南丁 on 2017/2/9.
+//  Copyright © 2017年 nandiin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slab.
+FOUNDATION_EXPORT double SlabVersionNumber;
+
+//! Project version string for Slab.
+FOUNDATION_EXPORT const unsigned char SlabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slab/PublicHeader.h>
+
+
